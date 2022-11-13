34ec03bdc5a2f07d5e6cc47235ad7004cd9a75b2
Atick Faisal <atickfaisal@gmail.com>
2022-11-13 13:12:51 +0300

    Update server url

--- network/src/main/kotlin/dev/atick/network/api/CardiacZoneApi.kt
+++ network/src/main/kotlin/dev/atick/network/api/CardiacZoneApi.kt
@@ -13,7 +13,7 @@ interface CardiacZoneApi {
         // const val BASE_URL = "https://stark-lowlands-43915.herokuapp.com"
         // const val BASE_URL = "http://192.168.1.102:5000"
         // const val BASE_URL = "http://18.222.190.174:8080"
-         const val BASE_URL = "http://34.170.139.48/"
+         const val BASE_URL = "http://34.133.239.54"
     }
 
     @POST("/user_login")

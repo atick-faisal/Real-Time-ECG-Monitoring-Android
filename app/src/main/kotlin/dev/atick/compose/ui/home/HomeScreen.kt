e757e1d5bcb774d181685f3e97cd110f4cc2079d
Atick Faisal <atickfaisal@gmail.com>
2022-12-01 18:30:08 +0300

    Implement QR code scanner

--- app/src/main/kotlin/dev/atick/compose/ui/home/HomeScreen.kt
+++ app/src/main/kotlin/dev/atick/compose/ui/home/HomeScreen.kt
@@ -1,23 +1,29 @@
 package dev.atick.compose.ui.home
 
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.QrCode
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.data.ScatterDataSet
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
 import dev.atick.compose.ui.BleViewModel
 import dev.atick.compose.ui.common.components.TopBar
 import dev.atick.compose.ui.home.components.EcgCard
@@ -39,31 +45,58 @@ fun HomeScreen(
         ScatterDataSet(listOf(), "R_PEAK")
     )
 
+    val scanner = remember { BarcodeScanning.getClient() }
+
+    val context = LocalContext.current
     val recordState by viewModel.recordState
     val abnormalEcgList by viewModel.abnormalEcgList.collectAsState(listOf())
+    val scaffoldState = rememberScaffoldState()
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.TakePicturePreview()
+    ) { bitmap ->
+        bitmap?.let {
+            val image = InputImage.fromBitmap(it, 0)
+            scanner.process(image).addOnSuccessListener { qrCodes ->
+                val qrCode = qrCodes.firstOrNull()
+                qrCode?.let { code ->
+                    val rawValue = code.rawValue ?: "NULL"
+                    Toast.makeText(
+                        context,
+                        "QR CODE: $rawValue",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
 
-    return Box(
-        Modifier
+    return Scaffold(
+        modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colors.background)
-    ) {
-        TopBar(
-            modifier = Modifier.align(Alignment.TopCenter),
-            title = "Dashboard",
-            onExitClick = onExitClick
-        )
-
-        Column(
-            modifier = Modifier
-                .padding(
-                    top = 80.dp,
-                    start = 16.dp,
-                    end = 16.dp,
-                    bottom = 16.dp
+            .background(MaterialTheme.colors.background),
+        topBar = {
+            TopBar(
+                title = "Dashboard",
+                onExitClick = onExitClick
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { launcher.launch(null) }) {
+                Icon(
+                    imageVector = Icons.Default.QrCode,
+                    contentDescription = "Scan QR"
                 )
-                .verticalScroll(rememberScrollState())
+            }
+        },
+        scaffoldState = scaffoldState
+    ) {
+        Column(
+            modifier = Modifier.verticalScroll(rememberScrollState())
         ) {
+            Spacer(modifier = Modifier.height(16.dp))
+
             HrCard(
+                modifier = Modifier.padding(horizontal = 16.dp),
                 averageHeartRate = averageHeartRate,
                 rrInterval = rrInterval
             )
@@ -71,6 +104,7 @@ fun HomeScreen(
             Spacer(modifier = Modifier.height(16.dp))
 
             EcgCard(
+                modifier = Modifier.padding(horizontal = 16.dp),
                 title = "Live ECG Signal",
                 data = EcgPlotData(
                     id = 0L,
@@ -84,7 +118,8 @@ fun HomeScreen(
             Button(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(48.dp),
+                    .height(48.dp)
+                    .padding(horizontal = 16.dp),
                 shape = RoundedCornerShape(16.dp),
                 onClick = { viewModel.record() }
             ) {
@@ -94,7 +129,11 @@ fun HomeScreen(
             Spacer(modifier = Modifier.height(16.dp))
 
             abnormalEcgList.forEach { ecgPlotData ->
-                EcgCard(title = ecgPlotData.getTimestamp(), data = ecgPlotData)
+                EcgCard(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    title = ecgPlotData.getTimestamp(),
+                    data = ecgPlotData
+                )
                 Spacer(modifier = Modifier.height(16.dp))
             }
         }

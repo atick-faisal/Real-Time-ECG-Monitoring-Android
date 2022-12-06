98bf643dde9dedb4ddf871d568eeeac12e23a61e
Atick Faisal <atickfaisal@gmail.com>
2022-12-06 15:05:15 +0300

    Fix start destination issue

--- movesense/src/main/kotlin/dev/atick/movesense/Movesense.kt
+++ movesense/src/main/kotlin/dev/atick/movesense/Movesense.kt
@@ -3,11 +3,12 @@ package dev.atick.movesense
 import dev.atick.movesense.data.ConnectionState
 import dev.atick.movesense.data.EcgSignal
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 
 interface Movesense {
     val heartRate: SharedFlow<Float>
     val ecgSignal: SharedFlow<EcgSignal>
-    val connectionState: SharedFlow<ConnectionState>
+    val connectionState: StateFlow<ConnectionState>
 
     suspend fun getMovesenseDeviceAddress(): String?
     fun initiateConnection(address: String)
